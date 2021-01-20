8c3712a73c81efa3f9705b82dd4f103fb694eae9
yunuseroglu <74877649+yunuseroglu@users.noreply.github.com>
2021-01-20 13:12:08 +0300

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+﻿<footer class="footer text-faded text-center py-5">
+    <div class="container">
+        <p class="m-0 small">Copyright &copy; KAHVEM 2021 YUNUS EROĞLU</p>
+    </div>
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
